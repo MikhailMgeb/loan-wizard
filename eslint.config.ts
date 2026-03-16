4154ef811aed5ef89679964e1fb124e88ee60f71
mgebrishvili.m <mgebrishvili.m@rwb.ru>
2026-03-16 20:36:27 +0300

    Remove unused assets (React and Vite SVGs) and update TypeScript configuration and dependencies for project alignment.

---
+++ eslint.config.ts
@@ -0,0 +1,17 @@
+import * as js from "@eslint/js";
+import * as pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import * as globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+	{
+		files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+		plugins: {js},
+		extends: ["js/recommended"],
+		languageOptions: {globals: globals.browser},
+	},
+	tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+
+]);
