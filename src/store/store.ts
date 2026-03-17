88f92977d63303da553ea34cfa4bc03efae838a9
mgebrishvili.m <mmd920802@gmail.com>
2026-03-17 13:48:35 +0300

    Add Redux store and loan form slice
    - Implement `loanFormSlice` with initial state, reducers, and actions for form steps and categories.
    - Integrate Redux store into the app with a provider.

---
+++ src/store/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { loanFormSlice } from "./loanForm/loanFormSlice.ts";
+
+export const store = configureStore({
+	reducer: {
+		loanForm: loanFormSlice.reducer,
+	},
+})
+
+export type TRootState = ReturnType<typeof store.getState>
+export type TAppDispatch = typeof store.dispatch
