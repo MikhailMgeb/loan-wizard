f9b0d97975814397cb3ab3cbe8577e7942afcd8a
mgebrishvili.m <mmd920802@gmail.com>
2026-03-17 18:25:17 +0300

    Remove unused `eslint-plugin-react` import from ESLint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,17 +1,14 @@
 import * as js from "@eslint/js";
-import * as pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 import * as globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
-	{
-		files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-		plugins: {js},
-		extends: ["js/recommended"],
-		languageOptions: {globals: globals.browser},
-	},
-	tseslint.configs.recommended,
-	pluginReact.configs.flat.recommended,
-
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: {js},
+    extends: ["js/recommended"],
+    languageOptions: {globals: globals.browser},
+  },
+  tseslint.configs.recommended,
 ]);
