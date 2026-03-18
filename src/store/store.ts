d2cc8f167515fb2bf8a08133a24073a4733047d5
mgebrishvili.m <mmd920802@gmail.com>
2026-03-18 12:35:34 +0300

    Simplify Redux hooks and CSS styles, update package versions
    - Removed `useAppSelector` due to unused state access in `store.ts`.
    - Adjusted `formWizard` CSS to remove unused `align-items`.
    - Locked `react-hook-form` and `react-redux` to specific versions.
    - Updated dependencies in `package-lock.json`.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { type TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { loanFormSlice } from "./loanForm/loanFormSlice.ts";
 
 export const store = configureStore({
@@ -11,4 +11,3 @@ export const store = configureStore({
 export type TRootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<TRootState> = useSelector
